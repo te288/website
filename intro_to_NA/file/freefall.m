9eb1d4931877fd2a8d83aff4c2d26d9125dced93
te288 <hirose65279@gmail.com>
2021-12-13 13:04:29 +0900

    Add 01_ODE.pdf

---
+++ intro_to_NA/file/freefall.m
@@ -0,0 +1,27 @@
+%% Input Parameters
+m    = 1.0; % mass of a small ball [kg]
+g    = 9.8; % gravitational acceleration [m/s^2]
+k    = 1.0; % coefficient of air resistance [N*s/m]
+v0   = 0.0; % Initial velocity [m/s]
+dt   = 0.1; % time step for simulation [s]
+t_max =  10; % time which simulation is stopped [s]
+
+%% Calculation
+t = 0;
+i = 1; % counter
+v_hist = [v0]; % array to hold velocity value
+while true
+    v = v_hist(i) + dt*(g - (k/m)*v_hist(i));
+    t = t + dt;
+    if t >= t_max
+        break
+    end
+    i = i+1;
+    v_hist = [v_hist v]; % this code is not actually good.
+end
+
+%% Plot result
+t_hist = 0:dt:t_max;
+plot(t_hist, v_hist);
+xlabel('t [s]');
+ylabel('v [m/s]');
