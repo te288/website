64664540e8fef421b89637c7a1996e98583ef60f
te288 <hirose65279@gmail.com>
2022-04-11 00:39:15 +0900

    解答ファイル修正

--- intro_to_NA/file/Advection_exact1D.m
+++ intro_to_NA/file/Advection_exact1D.m
@@ -2,16 +2,17 @@
 c = 1;
 t = 0;
 x = linspace(-3,10, 200);
-fig = figure;
-p = plot(x, exp(-(x-c*t).^2)); % Initial plot
-xlim([-3,10]);ylim([-0.1,1.1]);
+ax = subplot(1,1,1);
+plot(ax, x, exp(-(x-c*t).^2)); % Initial plot
+xlim(ax, [-3,10]);ylim(ax, [-0.1,1.1]);
 name = title(['t = ',num2str(t),'[s]']);
+hold(ax ,"on")
 
 
-for t = 0:0.2:15 % time data
+for t = 0:1:15 % time data
     name = title(['t = ',num2str(t,'%04.1f'),'[s]']);
-    p.YData = exp(-(x-c*t).^2);
-    drawnow;
-    filename = sprintf('plotEXACT%04.1f.png', t); % file name 
+    plot(ax, x, exp(-(x-c*t).^2));
+    hold(ax ,"on")
+    % filename = sprintf('plotEXACT%04.1f.png', t); % file name 
     %saveas(fig, filename); % save figure as png
 end
